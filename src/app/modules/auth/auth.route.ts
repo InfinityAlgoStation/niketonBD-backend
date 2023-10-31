a4f34e2fcf1d5649e2e9d1aa79e6c4044918d41d
sabbir1054 <mdsabbir1054@gmail.com>
2023-10-31 10:01:50 +0600

    authentication route validation add

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AuthController } from './auth.controller';
+import { AuthValidation } from './auth.validation';
 
 const router = express.Router();
 
 router.post('/register', AuthController.userRegistration);
-router.post('/login', AuthController.userLogin);
-router.post('/refreshToken', AuthController.refreshToken);
+router.post(
+  '/login',
+  validateRequest(AuthValidation.loginZodSchema),
+  AuthController.userLogin
+);
+router.post(
+  '/refreshToken',
+  validateRequest(AuthValidation.refreshTokenZodSchema),
+  AuthController.refreshToken
+);
 
 export const AuthRoutes = router;
